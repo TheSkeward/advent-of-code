ae7c774f60b55d558b13b8078bc0fe3c78a56e96
John Clark <johnnyclark90@gmail.com>
2020-12-21 17:34:09 -0800

    2015, Day 1

---
+++ 2015/advent01.py
@@ -0,0 +1,15 @@
+"""2015 Advent of Code, Day 1"""
+with open("input", "r+") as file:
+    puzzle_input = file.read()
+FLOOR = 0
+POSITION = 0
+BASEMENT = False
+for (index, character) in enumerate(puzzle_input):
+    if character == "(":
+        FLOOR += 1
+    if character == ")":
+        FLOOR -= 1
+    if not BASEMENT and FLOOR < 0:
+        BASEMENT = True
+        POSITION = index + 1
+print(FLOOR, POSITION)
